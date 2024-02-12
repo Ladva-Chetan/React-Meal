b7364d8954058e47df28e9386ea5e4a5a41b3acf
Ladva-Chetan <ladvachetan2105@gmail.com>
2024-02-12 11:35:27 +0530

    Progress to Add items to cart and outptuin it

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,13 +1,21 @@
+import { useContext } from "react";
 import Carticon from "../Cart/Carticon";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../assets/Store/cart-context";
+
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <Carticon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>5</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
